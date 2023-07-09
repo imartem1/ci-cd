07c076286882e0b248849bffd33df690fed1981e
akolesnikov <akolesnikov@team.amocrm.com>
2023-07-09 17:34:39 +0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 function App() {
   const [state, setState] = useState(0);
 
-  const increment = setState(prev => prev + 1);
+  const increment = () => setState(prev => prev + 1);
   return (
     <div className="App">
       <h1>value = {state}</h1>
-      <button onclick={increment}>
+      <button onClick={increment}>
       increment
       </button>
     </div>
